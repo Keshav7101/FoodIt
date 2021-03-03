6cf6a0d0a49fe1e797e741f48637c8a32d9c7e07
Keshav7101 <keshav.choudhary_cs18@gla.ac.in>
2021-03-04 00:09:45 +0530

    Added the Files

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is our application</Text>
+      <Text>Welcome to FoodIt</Text>
       <StatusBar style="auto" />
     </View>
   );
