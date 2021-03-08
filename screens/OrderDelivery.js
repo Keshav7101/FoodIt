33914a8a8fe08f893d985e3f67ebe8ba08be6a3f
Keshav7101 <keshav.choudhary_cs18@gla.ac.in>
2021-03-08 20:58:19 +0530

    Added the code to home restaurant  and orderdelivery

---
+++ screens/OrderDelivery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TextInput, StatusBar } from 'react-native';
+
+const OrderDelivery = () => {
+    return(
+        <View>
+            <Text>OrderDelivery</Text>
+        </View>
+    )
+}
+
+export default OrderDelivery;
---
+++ screens/Restaurant.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TextInput, StatusBar } from 'react-native';
+
+const Restaurant = () => {
+    return(
+        <View>
+            <Text>Restaurant</Text>
+        </View>
+    )
+}
+
+export default Restaurant;
