280b679b352f0ae24a5d821d685ce69c8ca04f41
Keshav7101 <keshav.choudhary_cs18@gla.ac.in>
2021-03-08 20:51:57 +0530

    Added view to home.js

---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TextInput, StatusBar } from 'react-native';
+
+const Home = () => {
+    return(
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
