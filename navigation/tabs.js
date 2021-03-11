eabd213995bff0a2c78f3ad4fade111839e49d83
Keshav7101 <keshav.choudhary_cs18@gla.ac.in>
2021-03-11 10:54:11 +0530

    Added code to the tabs folder

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,33 +1,99 @@
-import React from 'react';
-import {
-    View, Image, TouchableOpacity
-} from 'react-native'
-import {createBottomNavigator, BottomTabBar } from '@react-navigation/BottomTabBar';
-import {Home} from '../screens';
-import {COLORS, icons} from '../constants';
- const Tabs = createBottomNavigator();
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
- const Tabs = () => {
-     return(
-         <Tab.Navigator>
-             <Tab.Screens
-                name='Home'
-                component={Home}
-                options={{
-                    tabBarIcon:
-                        <Image
-                            source={icons.cutlery}
-                            resizeMode='contain'
-                            style={{
-                                width: 25,
-                                height: 25,
-                                tintColor: focused ? COLORS.primary : COLORS.secondary
+function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home Page</Text>
+    </View>
+  );
+}
 
-                            }}
-                        />
-                    }
-                }             
-             />
-         </Tab.Navigator>
-     )
- }
+function Profile() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Login Page</Text>
+    </View>
+  );
+}
+
+function Cart() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Cart</Text>
+    </View>
+  );
+}
+
+function Notifications() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Search Page</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#FFFFFF"
+      labelStyle={{ fontSize: 12 }}
+      style={{ backgroundColor: 'pink' }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="magnify" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          tabBarLabel: 'Cart',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="cart" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
