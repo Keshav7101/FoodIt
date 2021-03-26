f42064ab6a9bf354d9c0f1d80349fb238fcce02d
Keshav7101 <keshav.choudhary_cs18@gla.ac.in>
2021-03-26 18:51:06 +0530

    Added text to Suraj Dhaba screen

---
+++ Screens/SurajDhaba.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const DetailsScreen = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 , padding: 16}}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 25,
+              textAlign: 'center',
+              marginBottom: 16
+            }}>
+            Suraj Dhaba Food Here
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+export default DetailsScreen;
