ab377d30f6118917f0a09b04561393db40d2d8bd
Keshav7101 <keshav.choudhary_cs18@gla.ac.in>
2021-03-26 18:56:02 +0530

    Added the signup page

---
+++ Screens/Signup.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { View, Text,TouchableOpacity, Image, ScrollView, TextInput, StatusBar, Header, Button, ImageBackground } from 'react-native';
+
+const App = ({navigation}) => {
+  return (
+    <ScrollView>
+      <View>
+        <Image
+          source={{
+            uri: 'https://pbs.twimg.com/profile_images/530381904235610113/CVDbqftg_400x400.png',
+          }}
+          style={{ marginTop:20, marginLeft:30, width: 300, height: 150}}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 45,
+          width:280,
+          borderColor: 'black',
+          borderWidth: 2,
+          marginLeft: 30,
+          marginTop: 50,
+          padding: 10,
+          borderRadius:20
+        }}
+        placeholder= "  Username/Id"
+      />
+      
+      <TextInput
+        style={{
+          height: 45,
+          width:280,
+          borderColor: 'black',
+          borderWidth: 2,
+          marginLeft: 30,
+          marginTop: 20,
+          padding:10,
+          borderRadius:20
+        }}
+        placeholder= "  Email"
+      />
+
+      <TextInput
+        style={{
+          height: 45,
+          width:280,
+          borderColor: 'black',
+          borderWidth: 2,
+          marginLeft: 30,
+          marginTop: 20,
+          padding:10,
+          borderRadius:20
+        }}
+        placeholder= "  Password"
+      />
+      <TextInput
+        style={{
+          height: 45,
+          width:280,
+          borderColor: 'black',
+          borderWidth: 2,
+          marginLeft: 30,
+          marginTop: 20,
+          padding:10,
+          borderRadius:20
+        }}
+        placeholder= "  Mobile No."
+      />
+      <View>
+      <TouchableOpacity
+            style={{alignItems: 'center',
+            backgroundColor: '#DDDDDD',
+            padding: 10,
+            width: 280,
+            marginTop: 20,
+            marginLeft:30,
+            borderRadius:20
+          }}
+            onPress={
+              () => navigation.navigate('')
+            }>
+            <Text>SignUp</Text>
+          </TouchableOpacity>
+      </View>
+      <View>
+      <TouchableOpacity
+            style={{alignItems: 'center',
+            backgroundColor: '#DDDDDD',
+            padding: 10,
+            width: 280,
+            marginTop: 20,
+            marginLeft:30,
+            borderRadius:20
+          }}
+            onPress={
+              () => navigation.navigate('Login')
+            }>
+            <Text>Back to login</Text>
+          </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+}
+
+export default App;
