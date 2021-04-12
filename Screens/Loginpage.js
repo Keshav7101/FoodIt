0d47077d5f5491c13a331e0e677d93130c51a292
ankitpandey1787 <ankit.pandey_cs18@gla.ac.in>
2021-04-12 22:23:44 +0530

    Edited signup Page

--- Screens/Loginpage.js
+++ Screens/Loginpage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import styles from './styles';
+import styles from '../Styles/LoginpageStyle';
 import { firebase } from '../Screens/firebase/config'
 
 export default function LoginScreen({navigation}) {
