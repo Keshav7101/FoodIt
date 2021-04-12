fe699a9b0ab317212c896352deb6d4a2dcfad306
ankitpandey1787 <ankit.pandey_cs18@gla.ac.in>
2021-04-12 22:22:30 +0530

    Added styles for login page

--- Screens/Loginpage.js
+++ Screens/Loginpage.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
-import { firebase } from '../../firebase/config'
+import { firebase } from '../Screens/firebase/config'
 
 export default function LoginScreen({navigation}) {
     const [email, setEmail] = useState('')
