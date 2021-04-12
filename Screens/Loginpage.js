ac748d04c9c90b6b32688038489c251addb95095
ankitpandey1787 <ankit.pandey_cs18@gla.ac.in>
2021-04-12 22:20:39 +0530

    Added firebase config

--- Screens/Loginpage.js
+++ Screens/Loginpage.js
@@ -1,82 +1,81 @@
-import * as React from 'react';
-import {SafeAreaView, View,TouchableOpacity, Text, Image, ScrollView, TextInput } from 'react-native';
+import React, { useState } from 'react'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import styles from './styles';
+import { firebase } from '../../firebase/config'
 
-const App = ({navigation}) => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-    <ScrollView>
-      <View>
-        <Image
-          source={{
-            uri: 'https://pbs.twimg.com/profile_images/530381904235610113/CVDbqftg_400x400.png',
-          }}
-          style={{ marginTop:20, marginLeft:30, width: 300, height: 150, borderTopRightRadius:30,borderBottomLeftRadius:30}}
-        />
-      </View>
-      <TextInput
-        style={{
-          height: 45,
-          width:280,
-          borderColor: 'black',
-          borderWidth: 2,
-          marginLeft: 30,
-          marginTop: 70,
-          padding: 10,
-          borderRadius:20
-        }}
-        placeholder= "Loginid"
-      />
-      
-      <TextInput
-        style={{
-          height: 45,
-          width:280,
-          borderColor: 'black',
-          borderWidth: 2,
-          marginLeft: 30,
-          marginTop: 30,
-          padding:10,
-          borderRadius:20
-        }}
-        placeholder= "Password"
-      />
-      <View>
-      <TouchableOpacity
-            style={{alignItems: 'center',
-            justifyContent:'center',
-            backgroundColor: '#DDDDDD',
-            padding: 10,
-            width: 280,
-            marginTop: 20,
-            marginLeft:30,
-            borderRadius:20,
-          }}
-            onPress={
-              () => navigation.navigate('')
-            }>
-            <Text>Login</Text>
-          </TouchableOpacity>
-      </View>
-      <View style={{justifyContent:'center'}}>
-      <TouchableOpacity
-            style={{alignItems: 'center',
-            justifyContent:'center',
-            backgroundColor: '#DDDDDD',
-            padding: 10,
-            width: 280,
-            marginTop: 16,
-            marginLeft:30 ,
-            borderRadius:20
-          }}
-            onPress={
-              () => navigation.navigate('Signup')
-            }>
-            <Text>Signup</Text>
-          </TouchableOpacity>
-      </View>
-    </ScrollView>
-    </SafeAreaView>
-  );
+export default function LoginScreen({navigation}) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onFooterLinkPress = () => {
+        navigation.navigate('Registration')
+    }
+
+    const onLoginPress = () => {
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then((response) => {
+                const uid = response.user.uid
+                const usersRef = firebase.firestore().collection('users')
+                usersRef
+                    .doc(uid)
+                    .get()
+                    .then(firestoreDocument => {
+                        if (!firestoreDocument.exists) {
+                            alert("User does not exist anymore.")
+                            return;
+                        }
+                        const user = firestoreDocument.data()
+                        navigation.navigate('Home', {user: user})
+                    })
+                    .catch(error => {
+                        alert(error)
+                    });
+            })
+            .catch(error => {
+                alert(error)
+            })
+    }
+
+    return (
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+                <Image
+                    style={styles.logo}
+                    source={require('../../../assets/icon.png')}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='E-mail'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholderTextColor="#aaaaaa"
+                    secureTextEntry
+                    placeholder='Password'
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => onLoginPress()}>
+                    <Text style={styles.buttonTitle}>Log in</Text>
+                </TouchableOpacity>
+                <View style={styles.footerView}>
+                    <Text style={styles.footerText}>Don't have an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Sign up</Text></Text>
+                </View>
+            </KeyboardAwareScrollView>
+        </View>
+    )
 }
-
-export default App;
