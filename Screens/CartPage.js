ba0ff7ce89e43d1ac6c083d8380621cb31c9bad5
Keshav7101 <keshav.choudhary_cs18@gla.ac.in>
2021-04-15 03:39:06 +0530

    Added lottie to the order confirmation page

--- Screens/CartPage.js
+++ Screens/CartPage.js
@@ -1,27 +1,67 @@
-import * as React from 'react';
-import { View, Text, SafeAreaView,Button,TouchableOpacity } from 'react-native';
-
-const CartScreen = ({navigation}) => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1 , padding: 16}}>
-      <TouchableOpacity
-            style={{alignItems: 'center',
-            justifyContent:'center',
-            backgroundColor: '#DDDDDD',
-            padding: 10,
-            width: 280,
-            marginTop: 16,
-            marginLeft:30 ,
-            borderRadius:20
-          }}
-            onPress={
-              () => navigation.navigate('OrderConfirmation')
-            }>
-            <Text>Checkout</Text>
-          </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
-}
-export default CartScreen;
+import React from 'react';  
+//import react in our code.  
+import { StyleSheet, View, Button, TextInput } from 'react-native';  
+  
+export default class HomeScreen extends React.Component {  
+  
+    constructor(props) {  
+        //constructor to set default state  
+        super(props);  
+        this.state = {  
+            username: '',  
+        };  
+    }  
+    static navigationOptions = {  
+        title: 'Home',  
+        headerStyle: {  
+            backgroundColor: '#f4511e',  
+        },  
+        //headerTintColor: '#0ff',  
+        headerTitleStyle: {  
+            fontWeight: 'bold',  
+        },  
+    };  
+  
+    render() {  
+        const { navigate } = this.props.navigation;  
+        return (  
+            //View to hold our multiple components  
+            <View style={styles.container}>  
+            {/*Input to get the value from the user*/}  
+            <TextInput  
+        value={this.state.username}  
+        onChangeText={username => this.setState({ username })}  
+        placeholder={'Enter Any value'}  
+        style={styles.textInput}  
+        />  
+        <View style={styles.buttonStyle}>  
+            <Button  
+        title="Checkout"  
+        // color="#00B0FF"  
+        onPress={() =>  
+        this.props.navigation.navigate('OrderConfirmation', {  
+            userName: this.state.username,  
+        })  
+    }  
+        />  
+        </View>  
+        </View>  
+    );  
+    }  
+}  
+const styles = StyleSheet.create({  
+    container: {  
+        flex: 1,  
+        backgroundColor: '#fff',  
+        alignItems: 'center',  
+        padding: 16,  
+    },  
+    textInput: {  
+        height: 45,width: "95%",borderColor: "gray",borderWidth: 1,fontSize:20,  
+    },  
+    buttonStyle:{  
+        width: "93%",  
+        marginTop: 50,  
+        backgroundColor: "red",  
+    }  
+});
--- Screens/OrderConfirmation.js
+++ Screens/OrderConfirmation.js
@@ -1,21 +1,36 @@
 import * as React from 'react';
 import { View, Text, SafeAreaView,Button,TouchableOpacity,StyleSheet,Image } from 'react-native';
+import LottieView from 'lottie-react-native';
+
 const CartScreen = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1 , padding: 16}}>
-      
+        <LottieView 
+            key='animation'
+            autoPlay
+            resizeMode='cover'
+            source={require('../assets/cart-checkout.json')}
+            style={{height:300,marginLeft:8}}
+        />
+        <Text style={styles.confirmtext}>Your Order is Placed</Text>
+        <Text style={styles.placeconfirm}>Your Order Will be delivered at</Text>
       </View>
     </SafeAreaView>
   );
 }
-const Styles = StyleSheet.create({
-    sliderImage: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        resizeMode: 'cover',
-        height:100,
-        width:100,
+const styles = StyleSheet.create({
+    confirmtext:{
+        fontSize:20,
+        justifyContent:'center',
+        alignItems:'center',
+        marginLeft:70
+    },
+    placeconfirm:{
+        fontSize:20,
+        justifyContent:'center',
+        alignItems:'center',
+        marginLeft:20
     }
 })
 export default CartScreen;
