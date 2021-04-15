b081a1acc5a53d87f49ce5b96b7fa47fcb94ef1a
Sahil Gupta <43669801+sahilgupta2001@users.noreply.github.com>
2021-04-15 15:10:04 +0530

    Changed cart screen

--- Screens/CartPage.js
+++ Screens/CartPage.js
@@ -1,57 +1,78 @@
-import React from 'react';  
-//import react in our code.  
-import { StyleSheet, View, Button, TextInput } from 'react-native';  
-  
-export default class HomeScreen extends React.Component {  
-  
-    constructor(props) {  
-        //constructor to set default state  
-        super(props);  
-        this.state = {  
-            username: '',  
-        };  
-    };
+// React Native Pass Value From One Screen to Another Using React Navigation
+// https://aboutreact.com/react-native-pass-value-from-one-screen-to-another-using-react-navigation/
 
-    render() {  
-        const { navigate } = this.props.navigation;  
-        return (  
-            //View to hold our multiple components  
-            <View style={styles.container}>  
-            {/*Input to get the value from the user*/}  
-            <TextInput  
-        value={this.state.username}  
-        onChangeText={username => this.setState({ username })}  
-        placeholder={'Enter Any value'}  
-        style={styles.textInput}  
-        />  
-        <View style={styles.buttonStyle}>  
-            <Button  
-        title="Checkout"  
-        // color="#00B0FF"  
-        onPress={() =>  
-        this.props.navigation.navigate('OrderConfirmation', {  
-            userName: this.state.username,  
-        })  
-    }  
-        />  
-        </View>  
-        </View>  
-    );  
-    }  
-}  
-const styles = StyleSheet.create({  
-    container: {  
-        flex: 1,  
-        backgroundColor: '#fff',  
-        alignItems: 'center',  
-        padding: 16,  
-    },  
-    textInput: {  
-        height: 45,width: "95%",borderColor: "gray",borderWidth: 1,fontSize:20,  
-    },  
-    buttonStyle:{  
-        width: "93%",  
-        marginTop: 50,  
-        backgroundColor: "red",  
-    }  
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+} from 'react-native';
+
+const FirstPage = ({navigation}) => {
+  const [userName, setUserName] = useState('AboutReact');
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          React Native Pass Value From One Screen to Another
+          Using React Navigation
+        </Text>
+        <Text style={styles.textStyle}>
+          Please insert your name to pass it to second screen
+        </Text>
+        {/*Input to get the value from the user*/}
+        <TextInput
+          value={userName}
+          onChangeText={(username) => setUserName(username)}
+          placeholder={'Enter Any value'}
+          style={styles.inputStyle}
+        />
+        {/* On click of the button we will send the data as a Json
+          From here to the Second Screen using navigation */}
+        <Button
+          title="Go Next"
+          //Button Title
+          onPress={() =>
+            navigation.navigate('SecondPage', {
+              paramKey: userName,
+            })
+          }
+        />
+      </View>
+      <Text style={{textAlign: 'center', color: 'grey'}}>
+        www.aboutreact.com
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default FirstPage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  inputStyle: {
+    width: '80%',
+    height: 44,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#DBDBD6',
+  },
 });
